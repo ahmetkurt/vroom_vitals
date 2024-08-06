08c1790069a96f96f9597ba0d9df9b6673b52b32
Ahmet Kurt <ahmet.kurt@outlook.com.tr>
2024-08-06 19:46:55 +0200

    Initial commit

---
+++ src/vroom_vitals/__init__.py
@@ -0,0 +1,19 @@
+import obd
+
+connection = obd.OBD() # auto-connects to USB or RF port
+
+cmd = obd.commands.SPEED # select an OBD command (sensor)
+
+response = connection.query(cmd) # send the command, and parse the response
+
+print('+++++++++++++++++++++++++++++++')
+print("Ports", obd.scan_serial()) # ['/dev/ttyUSB0', '/dev/ttyUSB1']
+print("Is connected?", connection.is_connected())
+print("Port name", connection.port_name())
+print("Protocol ID", connection.protocol_id())
+print("Protocol name", connection.protocol_name())
+print("Connection status", connection.status())
+print('===============================')
+print(response.value) # returns unit-bearing values thanks to Pint
+print(response.value.to("kph")) # user-friendly unit conversions
+print(response.value.to("mph")) # user-friendly unit conversions
