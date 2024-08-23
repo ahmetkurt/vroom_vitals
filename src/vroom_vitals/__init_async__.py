52f05aa1e7fae8e2a01f3a3311fa194cfcf77c70
Ahmet Kurt <ahmet.kurt@outlook.com.tr>
2024-08-23 16:08:20 +0200

    Add working async example

--- src/vroom_vitals/__init_async__.py
+++ src/vroom_vitals/__init_async__.py
@@ -1,17 +1,36 @@
 import obd
 import time
 
-connection = obd.Async()
-print("Protocol ID", connection.protocol_id())
-print("Protocol name", connection.protocol_name())
-print("Supported commands", connection.supported_commands)
+def rpm_callback(r):
+    if not r.is_null():
+        print(f"RPM: {r.value} RPM")
+    else:
+        print("RPM: No data")
 
-command_rpm = obd.commands.RPM
-connection.watch(command_rpm)
+def pos_callback(r):
+    if not r.is_null():
+        print(f"POS: {r.value} POS")
+    else:
+        print("POS: No data")
+
+def bar_callback(r):
+    if not r.is_null():
+        print(f"BAR: {r.value} BAR")
+    else:
+        print("BAR: No data")
+
+def speed_callback(s):
+    if not s.is_null():
+        print(f"Speed: {s.value} km/h")
+    else:
+        print("Speed: No data")
+
+connection = obd.Async(fast=False, timeout=30)
+
+connection.watch(obd.commands.RPM, callback=rpm_callback)
+connection.watch(obd.commands.SPEED, callback=speed_callback)
+
 connection.start()
-
-response = connection.query(command_rpm)
-print("Response", response)
 
 time.sleep(5)
 
@@ -19,8 +38,7 @@ with connection.paused() as was_running:
     print("Connection paused...")
     print("Was running?", was_running)
 
-    command_throttle_pos = obd.commands.THROTTLE_POS
-    connection.watch(command_throttle_pos)
+    connection.watch(obd.commands.THROTTLE_POS, callback=pos_callback)
     print("New command THROTTLE_POS has been subscribed...")
 
 time.sleep(10)
@@ -29,5 +47,24 @@ with connection.paused() as was_running:
     print("Connection paused...")
     print("Was running?", was_running)
 
-    connection.unwatch(command_rpm)
+    connection.unwatch(obd.commands.RPM, callback=rpm_callback)
     print("Command RPM has been unsubscribed...")
+
+time.sleep(15)
+
+with connection.paused() as was_running:
+    print("Connection paused...")
+    print("Was running?", was_running)
+
+    connection.watch(obd.commands.BAROMETRIC_PRESSURE, callback=bar_callback)
+    print("New command BAROMETRIC_PRESSURE has been subscribed...")
+
+try:
+    while True:
+        # The main loop can do other things, or just pass
+        pass
+
+except KeyboardInterrupt:
+    print("Stopped by user")
+finally:
+    connection.stop()  # Stop the async connection
